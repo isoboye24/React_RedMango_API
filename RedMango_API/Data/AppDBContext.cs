88ae75d546c2a72796bfcad6c5b0296ec5121a80
isoboye24 <isobarguy4real@gmail.com>
2025-02-22 20:21:29 +0100

    I setup connection string and DB context.

---
+++ RedMango_API/Data/AppDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RedMango_API.Data
+{
+    public class AppDBContext:IdentityDbContext
+    {
+        public AppDBContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using RedMango_API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultDBConnection"));
+});
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDBContext>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
