edbf3c20272eb95bb00448f2fa065e79430227d7
isoboye24 <isobarguy4real@gmail.com>
2025-02-24 13:39:39 +0100

    I added authentication controller and Dependency Injection.

---
+++ RedMango_API/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using RedMango_API.Models.DTO;
+using RedMango_API.Services;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDBContext _db;
+        private ApiResponse _response;
+        private string secretKey;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public AuthController(AppDBContext db, IBlobService blobService, IConfiguration configuration, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+            _response = new ApiResponse();
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDTO registerModel)
+        {
+
+        }
+    }
+}
