802fc2a83b66912357cf486b352838d06bd908c8
isoboye24 <isobarguy4real@gmail.com>
2025-02-27 13:15:36 +0100

    I added OrderHeader and OrderDetail DTOs.

---
+++ RedMango_API/Models/DTO/OrderHeaderCreateDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RedMango_API.Models.DTO
+{
+    public class OrderHeaderCreateDTO
+    {
+        [Required]
+        public string PickUpName { get; set; }
+        [Required]
+        public string PickUpPhoneNumber { get; set; }
+        [Required]
+        public string PickUpEmail { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public double OrderTotal { get; set; }
+
+        public string StripePaymentIntendId { get; set; }
+        public string Status { get; set; }
+        public int TotalItems { get; set; }
+
+        public IEnumerable<OrderDetailCreateDTO> OrderDetailDTO { get; set; }
+    }
+}
---
+++ RedMango_API/Models/DTO/OrderHeaderUpdateDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace RedMango_API.Models.DTO
+{
+    public class OrderHeaderUpdateDTO
+    {
+        public int OrderHeaderId { get; set; }
+        public string PickUpName { get; set; }
+        public string PickUpPhoneNumber { get; set; }
+        public string PickUpEmail { get; set; }
+
+        public DateTime OrderDate { get; set; }
+        public string StripePaymentIntendId { get; set; }
+        public string Status { get; set; }
+    }
+}
